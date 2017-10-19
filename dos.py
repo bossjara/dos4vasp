e01a11b338108173ced20b0908b44cb8f98cc7fe
Nikolay Skorikov <nskorikov@gmail.com>
2017-10-19 10:00:54 +0500

    temporary commit. calc xps for surface Mn and Ge atmoms in slab

--- dos.py
+++ dos.py
@@ -8,11 +8,51 @@ def main():
     brtdos = np.zeros([npoints, 3], dtype=np.float)
     tdos[:, 0] = tdos[:, 0] - efermi
     write_dos(tdos, dosfile='tdos.dat')
+    doses = np.array(pdoses)
+    Mn_s = np.zeros([npoints, 3], dtype=np.float)
+    Mn_p = np.zeros([npoints, 3], dtype=np.float)
+    Mn_d = np.zeros([npoints, 3], dtype=np.float)
+    Ge_p = np.zeros([npoints, 3], dtype=np.float)
+    Mn_s[:, 0] = Mn_p[:, 0] = Mn_p[:, 0] = Mn_d[:, 0] = tdos[:, 0]
+    Ge_p[:, 0] = tdos[:, 0]
+    for i in range(doses.shape[1]):
+        Mn_s[i, 1] = doses[0, i, 1] + doses[1, i, 1]
+        Mn_s[i, 2] = doses[0, i, 2] + doses[1, i, 2]
+        Mn_p[i, 1] = doses[0, i, 3] + doses[0, i, 4] + doses[0, i, 5] +\
+                     doses[1, i, 3] + doses[1, i, 4] + doses[1, i, 5]
+        Mn_p[i, 2] = doses[0, i, 6] + doses[0, i, 7] + doses[0, i, 8] +\
+                     doses[1, i, 6] + doses[1, i, 7] + doses[1, i, 8]
 
+        Mn_d[i, 1] = doses[0, i, 9] + doses[0, i, 10] + doses[0, i, 11] +\
+                     doses[0, i, 12] + doses[0, i, 13] + doses[1, i, 9] + \
+                     doses[1, i, 10] + doses[1, i, 11] + doses[1, i, 12] + \
+                     doses[1, i, 13]
+        Mn_d[i, 2] = doses[0, i, 14] + doses[0, i, 15] + doses[0, i, 16] + \
+                     doses[0, i, 17] + doses[0, i, 18] + doses[1, i, 14] + \
+                     doses[1, i, 15] + doses[1, i, 16] + doses[1, i, 17] + \
+                     doses[1, i, 18]
+        Ge_p[i, 1] = doses[19, i, 6] + doses[19, i, 7] + doses[19, i, 8] +\
+                     doses[20, i, 6] + doses[20, i, 7] + doses[20, i, 8] + \
+                     doses[21, i, 6] + doses[21, i, 7] + doses[21, i, 8]
+        Ge_p[i, 2] = doses[19, i, 9] + doses[19, i, 10] + doses[19, i, 11] +\
+                     doses[20, i, 9] + doses[20, i, 10] + doses[20, i, 11] +\
+                     doses[21, i, 9] + doses[21, i, 10] + doses[21, i, 11]
     brtdos[:, 0] = tdos[:, 0]
     brtdos[:, 1] = calc_spectra(tdos[:, [0, 1]])
     brtdos[:, 2] = calc_spectra(tdos[:, [0, 2]])
     write_dos(brtdos, dosfile='brtdos.dat')
+    brtdos[:, 1] = calc_spectra(Mn_s[:, [0, 1]])
+    brtdos[:, 2] = calc_spectra(Mn_s[:, [0, 2]])
+    write_dos(brtdos, dosfile='Mn12-s.dat')
+    brtdos[:, 1] = calc_spectra(Mn_p[:, [0, 1]])
+    brtdos[:, 2] = calc_spectra(Mn_p[:, [0, 2]])
+    write_dos(brtdos, dosfile='Mn12-p.dat')
+    brtdos[:, 1] = calc_spectra(Mn_d[:, [0, 1]])
+    brtdos[:, 2] = calc_spectra(Mn_d[:, [0, 2]])
+    write_dos(brtdos, dosfile='Mn12-d.dat')
+    brtdos[:, 1] = calc_spectra(Ge_p[:, [0, 1]])
+    brtdos[:, 2] = calc_spectra(Ge_p[:, [0, 2]])
+    write_dos(brtdos, dosfile='Ge123-p.dat')
 
 
 def read_doscar(doscar='DOSCAR'):
@@ -66,7 +106,7 @@ def convolute(x, f, fbr):
     fsmear = np.zeros(len(x), dtype=np.float)
     for i in range(len(x)):
         for j in range(len(x)):
-            fsmear[i] += f[j] * fbr(x[j]-x[i], 0.05)
+            fsmear[i] += f[j] * fbr(x[j]-x[i], 0.1)
     return fsmear*np.pi/len(x)
 
 
